1f7b2d5c0785c3aded56c5c04744fbdbce74bd61
André Teixeira <andre-x8@hotmail.com>
2024-11-22 12:44:40 -0300

    feat:add novo teste

--- src/presentation/controllers/singup.spec.ts
+++ src/presentation/controllers/singup.spec.ts
@@ -16,4 +16,19 @@ describe("SignUp Controller", () => {
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new Error("Falta o parametro nome"));
   });
+
+  test("Shoud return 400 if no email is provide", () => {
+    //retorna um erro 400 em caso de o nome não for enviado
+    const sut = new SignUpController();
+    const httpRequest = {
+      body: {
+        name: "any_name",
+        password: "any_password",
+        passwordConfirmation: "any_password",
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new Error("Falta o parametro email"));
+  });
 });
--- src/presentation/controllers/singup.ts
+++ src/presentation/controllers/singup.ts
@@ -1,8 +1,17 @@
 export class SignUpController {
-  handle(httpReques: any): any {
-    return {
-      statusCode: 400,
-      body: new Error("Falta o parametro nome"),
-    };
+  handle(httpRequest: any): any {
+    if (!httpRequest.body.name) {
+      return {
+        statusCode: 400,
+        body: new Error("Falta o parametro nome"),
+      };
+    }
+
+    if (!httpRequest.body.email) {
+      return {
+        statusCode: 400,
+        body: new Error("Falta o parametro email"),
+      };
+    }
   }
 }
