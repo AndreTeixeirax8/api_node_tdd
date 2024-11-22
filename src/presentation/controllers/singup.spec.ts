c40a6a4c531565288c9d5507bfb61395059f4ec3
André Teixeira <andre-x8@hotmail.com>
2024-11-22 12:34:09 -0300

    feat: add mensagem de erro

--- src/presentation/controllers/singup.spec.ts
+++ src/presentation/controllers/singup.spec.ts
@@ -7,7 +7,6 @@ describe("SignUp Controller", () => {
     const sut = new SignUpController();
     const httpRequest = {
       body: {
-        // name: "any_name",
         email: "any_email@mail.com",
         password: "any_password",
         passwordConfirmation: "any_password",
@@ -15,5 +14,6 @@ describe("SignUp Controller", () => {
     };
     const httpResponse = sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new Error("Falta o parametro nome"));
   });
 });
--- src/presentation/controllers/singup.ts
+++ src/presentation/controllers/singup.ts
@@ -1,5 +1,8 @@
 export class SignUpController {
   handle(httpReques: any): any {
-    return {};
+    return {
+      statusCode: 400,
+      body: new Error("Falta o parametro nome"),
+    };
   }
 }
