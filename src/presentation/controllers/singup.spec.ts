0a01e94c39bb255e2c979096f0ea4b14171cbc36
André Teixeira <andre-x8@hotmail.com>
2024-12-14 13:11:13 -0300

    ajuste nos testes

--- src/presentation/controllers/singup.spec.ts
+++ src/presentation/controllers/singup.spec.ts
@@ -1,23 +1,32 @@
 import { InvalidParamError } from "../errors/invalid-param-error copy";
 import { MissingParamError } from "../errors/missing-param-error";
+import { EmailValidator } from "../protocols/email-validator";
 import { SignUpController } from "./singup";
 
-const makeSut = (): SignUpController => {
-  class EmailValidatorStub {
+interface SutTypes {
+  sut: SignUpController;
+  emailValidatorStub: EmailValidator;
+}
+
+const makeSut = (): any => {
+  class EmailValidatorStub implements EmailValidator {
     isValid(email: string): boolean {
-      return true;
+      return false;
     }
   }
 
   const emailValidatorStub = new EmailValidatorStub();
+  const sut = new SignUpController(emailValidatorStub);
 
-  return new SignUpController(emailValidatorStub);
+  return {
+    sut,
+    emailValidatorStub,
+  };
 };
 
 describe("SignUp Controller", () => {
   test("Shoud return 400 if no name is provide", () => {
-    //retorna um erro 400 em caso de o nome não for enviado
-    const sut = makeSut();
+    const { sut } = makeSut();
     const httpRequest = {
       body: {
         email: "any_email@mail.com",
@@ -27,12 +36,11 @@ describe("SignUp Controller", () => {
     };
     const httpResponse = sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
-    expect(httpResponse.body).toEqual(new MissingParamError("nome"));
+    expect(httpResponse.body).toEqual(new MissingParamError("name"));
   });
 
   test("Shoud return 400 if no email is provide", () => {
-    //retorna um erro 400 em caso de o nome não for enviado
-    const sut = makeSut();
+    const { sut } = makeSut();
     const httpRequest = {
       body: {
         name: "any_name",
@@ -46,12 +54,11 @@ describe("SignUp Controller", () => {
   });
 
   test("Shoud return 400 if no password is provide", () => {
-    //retorna um erro 400 em caso de o password não for enviado
-    const sut = makeSut();
+    const { sut } = makeSut();
     const httpRequest = {
       body: {
         name: "any_name",
-        password: "any_password",
+        email: "any_email@mail.com",
         passwordConfirmation: "any_password",
       },
     };
@@ -61,12 +68,12 @@ describe("SignUp Controller", () => {
   });
 
   test("Shoud return 400 if an invalid email is is provided", () => {
-    //retorna um erro 400 em caso de o password não for enviado
-    const sut = makeSut();
+    const { sut, emailValidatorStub } = makeSut();
+    jest.spyOn(emailValidatorStub, "isValid").mockReturnValueOnce(false);
     const httpRequest = {
       body: {
         name: "any_name",
-        email: "any_email@mail.com",
+        email: "invalid_email",
         password: "any_password",
         passwordConfirmation: "any_password",
       },
--- src/presentation/controllers/singup.ts
+++ src/presentation/controllers/singup.ts
@@ -2,8 +2,16 @@ import { HttpRequest, HttpResponse } from "../protocols/http";
 import { MissingParamError } from "../errors/missing-param-error";
 import { badRequest } from "../helpers/http-helpers";
 import { Controller } from "../protocols/controller";
+import { EmailValidator } from "../protocols/email-validator";
+import { InvalidParamError } from "../errors/invalid-param-error copy";
 
 export class SignUpController implements Controller {
+  private readonly emailValidator: EmailValidator;
+
+  constructor(emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator;
+  }
+
   handle(httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ["name", "email", "password"];
     for (const field of requiredFields) {
@@ -11,5 +19,16 @@ export class SignUpController implements Controller {
         return badRequest(new MissingParamError(field));
       }
     }
+
+    const isValid = this.emailValidator.isValid(httpRequest.body.email);
+    if (!isValid) {
+      return badRequest(new InvalidParamError("email"));
+    }
+
+    // Retorno para o caso de sucesso
+    return {
+      statusCode: 200,
+      body: { message: "Success" },
+    };
   }
 }
