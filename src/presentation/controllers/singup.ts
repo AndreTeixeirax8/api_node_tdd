213df22e6b8e1429f58542f21b6f0b6c76c26332
André Teixeira <andre-x8@hotmail.com>
2025-01-04 09:11:25 -0300

    add teste de server error

--- src/presentation/controllers/singup.ts
+++ src/presentation/controllers/singup.ts
@@ -4,6 +4,7 @@ import { badRequest } from "../helpers/http-helpers";
 import { Controller } from "../protocols/controller";
 import { EmailValidator } from "../protocols/email-validator";
 import { InvalidParamError } from "../errors/invalid-param-error copy";
+import { ServerError } from "../errors/server-error";
 
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator;
@@ -13,22 +14,29 @@ export class SignUpController implements Controller {
   }
 
   handle(httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = ["name", "email", "password"];
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field));
+    try {
+      const requiredFields = ["name", "email", "password"];
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field));
+        }
       }
-    }
 
-    const isValid = this.emailValidator.isValid(httpRequest.body.email);
-    if (!isValid) {
-      return badRequest(new InvalidParamError("email"));
-    }
+      const isValid = this.emailValidator.isValid(httpRequest.body.email);
+      if (!isValid) {
+        return badRequest(new InvalidParamError("email"));
+      }
 
-    // Retorno para o caso de sucesso
-    return {
-      statusCode: 200,
-      body: { message: "Success" },
-    };
+      // Retorno para o caso de sucesso
+      return {
+        statusCode: 200,
+        body: { message: "Success" },
+      };
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: new ServerError(),
+      };
+    }
   }
 }
